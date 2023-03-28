36313e827c42dc77e88ef4b7183b8803e343a0af
Vishali Vallioor <vishalivallioor@vishalis-mbp.lan>
2023-03-27 23:22:04 -0700

    added html, css, js files

---
+++ portfolio-site/site.js
@@ -0,0 +1,18 @@
+
+  // Get all the buttons
+  const buttons = document.querySelectorAll('.button');
+  
+  // Add a click event listener to each button
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      // Toggle the clicked class on the button
+      button.classList.toggle('clicked');
+    });
+  });
+
+const experiences = document.querySelectorAll('.experience');
+const colors = ['color1', 'color2', 'color3', 'color4'];
+
+experiences.forEach((experience, index) => {
+  experience.classList.add(colors[index]);
+});
