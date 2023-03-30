7ce17d53713884a5bfb27036eefba7438c684f18
Siddharth Nath <sidnath@berkeley.edu>
2023-03-30 16:15:19 -0700

    Updates

--- portfolio-site/site.js
+++ portfolio-site/site.js
@@ -1,14 +1,14 @@
 
-  // Get all the buttons
-  const buttons = document.querySelectorAll('.button');
-  
-  // Add a click event listener to each button
-  buttons.forEach(button => {
-    button.addEventListener('click', () => {
-      // Toggle the clicked class on the button
-      button.classList.toggle('clicked');
-    });
+// Get all the buttons
+const buttons = document.querySelectorAll('.button');
+
+// Add a click event listener to each button
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Toggle the clicked class on the button
+    button.classList.toggle('clicked');
   });
+});
 
 const experiences = document.querySelectorAll('.experience');
 const colors = ['color1', 'color2', 'color3', 'color4'];
